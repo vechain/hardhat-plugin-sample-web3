c9574de6fcba76e1f057715a21982f7e8cef2a29
MakisChristou <makis_christou@protonmail.com>
2023-01-19 11:31:28 +0200

    Sample project

---
+++ README.md
@@ -0,0 +1,15 @@
+# Sample Hardhat Project using thor
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract and a test for that contract.
+
+First, install the node modules:
+
+```bash
+npm install
+```
+
+Before running the tests, you need to spin up a local thor instance. Assuming that thor is running on localhost run the sample test on the `Storage.sol` contract.
+
+```bash
+npx hardhat test --network vechain
+```
---
+++ test/sample/Storage.test.js
@@ -0,0 +1,39 @@
+
+  const Storage = artifacts.require('Storage');
+  const { expect } = require('chai');
+
+  contract('Storage', function (accounts) {
+    beforeEach(async function () {
+      this.Storage = await Storage.new({ from: accounts[0] });
+    });
+
+    it('default value is 0', async function () {
+        var ret = await this.Storage.retrieve();
+        expect(String(ret)).to.eql('0');
+      });
+
+
+      it('store/retrieve is working', async function () {
+        await this.Storage.store(15);
+        var ret = await this.Storage.retrieve();
+        expect(String(ret)).to.eql('15');
+      });
+
+      it('emits a storage event', async function () {
+        const ret = await this.Storage.store(15)
+        expect(ret.logs[0].event).to.be.equal("StoreEvent")
+      });
+
+      it('revert if num > 100', async function () {
+
+        try
+        {
+          await this.Storage.store(1000);
+        } catch (err)
+        {
+          const msg = err.reason;
+          expect(msg).to.eql("Number must be < 100");
+        }
+      });
+  });
+  
